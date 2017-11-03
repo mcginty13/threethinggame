074c675e59b89785138ced754429a4b51b1f5787
Daniel Hamilton-McGinty <daniel.hamilton.mcginty@gmail.com>
2017-11-03 23:39:54 +0000

    init

---
+++ Assets/TimeDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TimeDestroyer : MonoBehaviour
+    {
+    void Start()
+        {
+            Invoke("DestroyObject", LifeTime);
+        }
+
+        void DestroyObject()
+        {
+            if (GameManager.Instance.GameState != GameState.Dead)
+                Destroy(gameObject);
+        }
+
+        public float LifeTime = 10f;
+    }
---
+++ Assets/assignmaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignmaterial : MonoBehaviour {
+
+    public Texture2D[] myTextures;
+    
+	// Use this for initialization
+	void Start () {
+        string lr = gameObject.name;
+        myTextures = Resources.LoadAll<Texture2D>("textures\\"+lr);
+        
+        GetComponent<Renderer>().material.SetTexture("_MainTex", myTextures[Random.Range(0, myTextures.Length)]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
