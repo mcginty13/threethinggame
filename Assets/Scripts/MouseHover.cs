33c30b0f54998bc45cc203de8eac06771be40f84
Daniel Hamilton-McGinty <daniel.hamilton.mcginty@gmail.com>
2017-11-04 00:16:02 +0000

    stevies tings

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        GetComponent<Transform> ().localScale = new Vector3(1.2f, 1.2f, 1);
+        GetComponent<TextMesh>().color = new Color(1, 0, 0, 1);
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        GetComponent<TextMesh>().color = new Color(0, 0, 0, 1);
+    }
+
+
+    public bool isStart;
+    public bool isQuit;
+    void OnMouseDown()
+    {
+        if(isStart)
+        {
+            GetComponent<TextMesh>().text = "Loading";
+        }
+        if (isQuit)
+        {
+            return;
+        }
+        
+    }
+
+}
